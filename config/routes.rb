d4ab6f35c5002e72fc5ed6f365e5017848b1c63e
Dylan Palmboom <nofxboy1234@gmail.com>
2022-11-14 21:02:12 +0200

    Coded up to https://guides.rubyonrails.org/getting_started.html 7.3.2 - Using Strong Parameters

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@
 Rails.application.routes.draw do
   # Defines the root path route ("/")
   root 'articles#index'
-
-  get '/articles', to: 'articles#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # get '/articles', to: 'articles#index'
+  # get '/articles/:id', to: 'articles#show'
+  resources :articles
 end
