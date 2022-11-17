f99b586a202c6b576bd8ed24a3e3c47b5d25ad7c
Dylan Palmboom <nofxboy1234@gmail.com>
2022-11-17 11:42:43 +1000

    Update to use new and create

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,15 @@
 # frozen_string_literal: true
 
+#         root  GET    /                            articles#index
+#     articles  GET    /articles(.:format)          articles#index
+#               POST   /articles(.:format)          articles#create
+#  new_article  GET    /articles/new(.:format)      articles#new
+# edit_article  GET    /articles/:id/edit(.:format) articles#edit
+#      article  GET    /articles/:id(.:format)      articles#show
+#               PATCH  /articles/:id(.:format)      articles#update
+#               PUT    /articles/:id(.:format)      articles#update
+#               DELETE /articles/:id(.:format)      articles#destroy
+
 # ArticlesController
 class ArticlesController < ApplicationController
   def index
@@ -15,12 +25,18 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    @article = Article.new(title: '...', body: '...')
+    @article = Article.new(article_params)
 
     if @article.save
       redirect_to @article
     else
       render :new, status: :unprocessable_entity
     end
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :body)
   end
 end
