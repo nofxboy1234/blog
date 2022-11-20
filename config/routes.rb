5035b62324dcef248cebf1cfddfc641f781971cd
Dylan Palmboom <nofxboy1234@gmail.com>
2022-11-20 21:04:22 +1000

    Let user add comments

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # get '/articles', to: 'articles#index'
   # get '/articles/:id', to: 'articles#show'
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
 end
